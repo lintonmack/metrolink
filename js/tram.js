6a8e175f7335423de49b7bc67f209cc718e45606
Paul <ramshaw@hotmail.co.uk>
2017-07-03 20:41:01 +0100

    added tramand drive functions

---
+++ js/tram.js
@@ -0,0 +1,25 @@
+function Tram (route, startingStation) {
+    this._route = route
+    this._currentStation = startingStation
+}
+
+Tram.prototype = {
+    getCurrentStation: function () {
+        return this._currentStation
+    },
+    drive: function() {
+        var routeStations = this._route.getStations()
+        var currentStationIndex = routeStations.indexOf(this._currentStation)
+        var nextStationIndex = currentStationIndex + 1
+console.log(currentStationIndex)
+console.log(nextStationIndex.length)
+console.log(routeStations)
+console.log(routeStations.length)
+        if (nextStationIndex >= routeStations.length) {
+            return 'End of line!'
+        }
+        this._currentStation = routeStations[nextStationIndex]
+
+        return 'Now arriving at: ' + this._currentStation.getName()
+    }
+}
